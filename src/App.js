4208cc3be0881bfb9b5f270c89af257312e581fd
ianlee92 <ethian9227@gmail.com>
2021-05-21 11:58:58 +0900

    SDK 초기화 한 번만

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
+import React from 'react';
 
+class App extends React.Component {
+  render() {
+    // 등록된 앱의 JavaScript key
+    const jsKey = "63458f6f9c843d7aa1b95d9890bb0b69";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // SDK는 한 번만 초기화해야 한다.
+    // 중복되는 초기화를 막기 위해 isInitialized()로 SDK 초기화 여부를 판단한다.
+    if (!window.Kakao.isInitialized()) {
+      // JavaScript key를 인자로 주고 SDK 초기화
+      window.Kakao.init(jsKey);
+      // SDK 초기화 여부를 확인하자.
+      console.log(window.Kakao.isInitialized());
+    }
+    return (
+      <h1>kakologin</h1>
+    )
+  }
 }
 
 export default App;
